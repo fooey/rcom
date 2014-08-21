9c71f0ffdf1ea921083491a80d5e6a6db54fcbaa
Jason Rushton <github.com@schtuph.com>
2014-08-21 10:37:57 -0600

    redirect lost /font requests

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
+app.use('/fonts/:path', function(req, res) {
+	res.redirect(301, 'http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/fonts/' + req.params.path)
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Listening on port ' + port);
