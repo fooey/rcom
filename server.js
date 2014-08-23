0072363ff873f0b725b8e993b9ded41668f35083
Jason Rushton <github.com@schtuph.com>
2014-08-22 21:09:16 -0600

    use jade to render the index; split templates into separate files that are included

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-if(process.env.PORT !== 'development') {
-	require('newrelic');
-}
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+
+app.set('view engine', 'jade')
+if(process.env.PORT !== 'development') {
+	require('newrelic');
+	app.set('env', 'development');
+}
+else {
+	app.set('env', 'production');
+	app.set('view cache', true)
+}
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/fonts/:path', function(req, res) {
 	res.redirect(301, 'http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/fonts/' + req.params.path)
 });
+app.use('/', function(req, res) {res.render('index')});
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port);
